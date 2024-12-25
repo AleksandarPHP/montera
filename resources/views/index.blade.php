60f4be34a16f8fba5184ae05abee2ce6fdca1780
Aleksandarjo <aleksandar.joovanoovic@gmail.com>
2024-12-26 00:45:49 +0100

    Add Footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,9 @@
-@extends('layouts.app')
-    @section('content')
-    
-    @endsection
+@include('partials.header')
+
+<main>
+    <section class="hero" style="min-height: 100dvh">
+        <div class="container">
+            <h1>hero video</h1>
+        </div>
+    </section>
+</main>
