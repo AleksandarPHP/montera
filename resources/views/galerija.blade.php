a7fb7d546d1bb7b132d0c996d784d2af3ce900c9
AleksandarPHP <acocoaj123@gmail.com>
2025-11-17 17:23:41 +0100

    fix: gallery

--- resources/views/galerija.blade.php
+++ resources/views/galerija.blade.php
@@ -6,23 +6,29 @@
             @foreach ($albums as $album)
             <div class="wrapper">
                 <h2 class="subtitle">{{$album->title}}</h2>
-                <div @class([
+                <div
+                 {{-- @class([
                     'building-slider' => $album->type == 1,
                     'location-slider' => $album->type == 2,
                     'apartments-slider' => $album->type == 3,
-                ])>
+                ]) --}}
+                >
+                <div class="row">
                     @foreach ($album->gallery as $item)
+                    <div class="col-2">
                     <a
                         href="{{asset('storage/'.$item->image)}}"
                         data-fancybox="building"
-                        data-caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                        data-caption="{{ $album->title }}"
                     >
                         <img
-                            class="img-fluid"
-                            src="{{asset('storage/'.$item->image)}}"
+                            {{-- class="img-fluid" --}}
+                            src="{{asset('storage/'.$item->image)}}" width="220px" height="220px"
                         />
                     </a>
+                    </div>
                     @endforeach
+                </div>
                 </div>
             </div>
             @endforeach
